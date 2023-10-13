6a701efa2d68dfc4c11fb004fe9de3924555e1d0
Odinson4 <odinson.da@gmail.com>
2023-10-12 19:30:22 -0600

    if it works it works. Fixed the N/A and now have a functional CSV file

--- src/scraping/sheriff.py
+++ src/scraping/sheriff.py
@@ -8,8 +8,11 @@ base_url = 'https://salesweb.civilview.com/'
 # Define the URL of the sales search page
 search_url = f'{base_url}Sales/SalesSearch?countyId=2'
 
+# Create a session to maintain state
+session = requests.Session()
+
 # Send an HTTP GET request to the search page
-response = requests.get(search_url)
+response = session.get(search_url)
 
 # Check if the request was successful
 if response.status_code == 200:
@@ -24,68 +27,50 @@ if response.status_code == 200:
         scraped_data = []
 
         for details_link in details_links:
+            # Initialize a dictionary to store data for each detail page
+            data = {}
+
             # Extract the href attribute from the <a> tag to get the detail URL
             detail_url = f'{base_url}{details_link.get("href")}'
 
-            # Send an HTTP GET request to the sales detail page
-            response = requests.get(detail_url)
+            # Send an HTTP GET request to the sales detail page using the session
+            response = session.get(detail_url)
 
             print('Detail URL:', detail_url)
 
             # Check if the request was successful
             if response.status_code == 200:
                 # Parse the HTML content of the detail page using BeautifulSoup
-                soup = BeautifulSoup(response.text, 'html.parser')
+                detail_soup = BeautifulSoup(response.text, 'html.parser')
 
                 # Helper function to extract text from an element or return 'N/A' if not found
                 def extract_text(element):
                     return element.get_text(strip=True) if element else 'N/A'
 
                 # Find all the rows in the table
-                rows = soup.find_all('tr')
-
-                print ('rows:', rows)
-                print ('len(rows):', len(rows))
-                print ('rows[0]:', rows[0])
-
-                # Initialize variables to store the scraped data for each detail page
-                sheriff_number = court_case_number = sales_date = plaintiff = defendant = address = description = approximate_upset = attorney = attorney_phone = 'N/A'
+                rows = detail_soup.find_all('tr')
 
                 # Iterate through the rows and extract data based on the labels
                 for row in rows:
-                    label = row.find('td')
-                    if label:
-                        label_text = label.get_text(strip=True)
-                        data_cell = label.find_next_sibling('td')
+                    # Extract the text from the first column (label)
+                    label = extract_text(row.find('td', class_='heading-bold columnwidth-15'))
 
-                        if data_cell:
-                            data_text = data_cell.get_text(strip=True)
-                        else:
-                            data_text = 'N/A'
+                    # Find the second <td> element within the current row containing data
+                    value_element = row.find_all('td')[1] if len(row.find_all('td')) > 1 else None
 
-                        # Print the label and data for debugging
-                        print(f'Label: {label_text}')
-                        print(f'Data: {data_text}')
+                    # Extract the text from the second <td> element
+                    value = extract_text(value_element)
 
-                # Create a dictionary to store the extracted data for the current detail page
-                data = {
-                    'Sheriff #': sheriff_number,
-                    'Court Case #': court_case_number,
-                    'Sales Date': sales_date,
-                    'Plaintiff': plaintiff,
-                    'Defendant': defendant,
-                    'Address': address,
-                    'Description': description,
-                    'Approx. Upset': approximate_upset,
-                    'Attorney': attorney,
-                    'Attorney Phone': attorney_phone
-                }
+                    print(f'{label}: {value}')
 
-                print ('Data:', data)
-                print ('row:', row)
+                    # Add the data to the dictionary
+                    data[label] = value
 
-                # Append the data to the scraped_data list
+                # Append the data dictionary to the scraped_data list
                 scraped_data.append(data)
+
+                print('Data extracted:', data)
+
             else:
                 print(f'Failed to retrieve the detail page for URL: {detail_url}. Status code:', response.status_code)
 
